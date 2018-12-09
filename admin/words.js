d91be0b2393e1ed9ab32f6a0fa57dac4cdf8d3e9
RPerkuhn <ralf.perkuhn@online.de>
2018-12-09 15:04:50 +0100

    internationalisation

---
+++ admin/words.js
@@ -0,0 +1,11 @@
+'use strict';
+
+systemDictionary = {
+  "enter your VW Car-Net username here": {"en": "enter your VW Car-Net username here", "de": "VW Car-Net Benutzername (eMail)"},
+  "enter your VW Car-Net password here": {"en": "enter your VW Car-Net password here", "de": "VW Car-Net Kennwort"},
+  "enter your VIN here (optional)": {"en": "enter your VIN here (optional)", "de": "Ggf. Fahrgestellnummer (optional)"},
+  "Retrieve eManager information:": {"en": "Retrieve eManager information:", "de": "Abfragen von eManager Informationen:"},
+  "Retrieve climater information:": {"en": "Retrieve climater information", "de": "Abfragen von Lüftung/Klimaanlage Informationen:"},
+  "Retrieve location information:": {"en": "Retrieve location information:", "de": "Abfragen von Standort Informationen:"},
+  "enter your Google API Key here (optional for getting more location addressinfo)": {"en": "enter your Google API Key here (optional for getting more location addressinfo)", "de": "Google API Key (optional zum Ermittlen der Adresse aus den Geodaten)"}
+};
